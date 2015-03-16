e7aac2fb7ba40dad034cf218894b31114a0e9cde
josephj <josephj6802@gmail.com>
2015-03-16 11:56:30 +1100

    Use CommonJS for right now

--- dist/alignme.js
+++ dist/alignme.js
@@ -1,3 +1,6 @@
+// DON'T MODIFY THIS FILE!
+// MODIFY ITS SOURCE FILE!
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /*global window, $, define, document */
 /**
  * A JavaScript utility which automatically aligns position of an overlay.
@@ -203,3 +206,5 @@ if (window.Stackla) { // Vanilla JS
     window.AlignMe = AlignMe;
 }
 
+
+},{}]},{},[1]);
